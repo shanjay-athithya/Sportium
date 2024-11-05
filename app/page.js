589449a78d9eb0ded219c4f416133f7eb566476d
shanjay-athithya <shanjayathithya2004@gmail.com>
2024-11-05 18:31:23 +0530

    Club Benefits

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@ import './globals.css'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
 import FeaturedSports from '../components/FeaturedSports';
+import ClubBenefits from '../components/ClubBenefits';
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Header />
       <Hero />
       <FeaturedSports />
+      <ClubBenefits />
     </>
   );
 }
