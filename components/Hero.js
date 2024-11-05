bbdfc3d6c2a7a5bf9ad54f5a302d8b360f779399
shanjay-athithya <shanjayathithya2004@gmail.com>
2024-11-05 15:28:18 +0530

    Hero resoved

--- components/Hero.js
+++ components/Hero.js
@@ -8,7 +8,7 @@ export default function Hero() {
         <div className="container mx-auto text-center text-white px-4 md:px-0">
           <h2 className="text-4xl md:text-6xl font-bold mb-4">Welcome to the SSN Sportium</h2>
           <p className="text-3xl font-bold text-darkGrey md:text-2xl mb-8">
-            "Together we compete, Together we conquer"
+            Together we compete, Together we conquer
           </p>
           <div className="flex justify-center space-x-4">
             <button className="bg-transparent border border-white py-3 px-6 rounded font-bold hover:bg-white hover:text-primaryOrange transition duration-200">
