9da1dc96174f1792bdb6b35772e60681a8f2eb10
shanjay-athithya <shanjayathithya2004@gmail.com>
2024-12-23 20:56:02 +0530

    events

--- components/Home/Events.js
+++ components/Home/Events.js
@@ -36,44 +36,56 @@ export default function Events() {
   ];
 
   return (
-    <section className="bg-darkGrey py-16">
+    <section id="events"  className="bg-darkGrey py-16">
       <div className="container mx-auto px-6 p-10 text-center">
-        <h3 className="text-4xl font-extrabold text-primaryOrange mb-16">Events</h3>
-        
-        {/* Horizontal Scrollable Events */}
-        <div className="flex overflow-x-auto space-x-10 pb-4 scrollbar-hide">
-          {events.map(event => (
+        <h3 className="text-4xl font-extrabold text-primaryOrange mb-16">
+          Events
+        </h3>
+
+        <div
+          className={`${
+            events.length <= 2
+              ? "flex justify-center gap-8 flex-wrap"
+              : "flex gap-8 md:gap-16 overflow-x-auto scrollbar-hide"
+          }`}
+        >
+          {events.map((event) => (
             <div
               key={event.id}
-              className={`flex-shrink-0 bg-white rounded-lg p-6 shadow-lg transition duration-300 border-l-8 ${
-                event.isCurrent ? 'border-primaryOrange' : 'border-gray-500'
-              }`}
-              style={{
-                width: '87vw', // Adjust for mobile
-                maxWidth: '400px', // Max width for larger screens
-                height: '400px', // Adjust height as needed
-              }}
+              className="flex-shrink-0 w-[87vw] sm:w-[400px] md:w-[500px] h-[450px] sm:h-[500px] relative group rounded-lg overflow-hidden shadow-xl hover:shadow-2xl transition duration-300 transform hover:scale-105 bg-darkGrey"
             >
-              {/* Icon Box */}
-              <div className="mb-6">
+              {/* Image Section */}
+              <div className="relative h-[75%] overflow-hidden">
                 <Image
                   src={event.icon}
-                  alt={`${event.title} icon`}
-                  width={200} // Set width for the icon
-                  height={200} // Set height for the icon
-                  className="mx-auto"
+                  alt={event.title}
+                  layout="fill"
+                  objectFit="cover"
+                  className="transition-transform duration-300 group-hover:scale-110 opacity-70"
                 />
+                <div className="absolute inset-0 bg-gradient-to-t from-black to-transparent opacity-60 group-hover:opacity-80 transition duration-300"></div>
               </div>
-              <h4 className="text-2xl font-semibold mb-4 text-primaryOrange">
-                {event.title}
-              </h4>
-              <p className="text-sm mb-2 text-gray-800">
-                <span className="font-bold">Date:</span> {event.dateRange}
-              </p>
-              <p className="mb-4 text-gray-700">{event.description}</p>
-              <p className="text-sm text-gray-800">
-                <span className="font-bold">Location:</span> {event.location}
-              </p>
+
+              {/* Content Section */}
+              <div className="absolute bottom-0 w-full px-4 sm:px-6 py-2 sm:py-3 bg-primaryOrange bg-opacity-90 text-left h-[45%]">
+                <h4 className="text-lg sm:text-3xl font-bold text-white mb-3 sm:mb-3">
+                  {event.title}
+                </h4>
+                <p className="text-xl font-bold sm:text-md text-darkGrey mb-2">
+                  Date: {event.dateRange}
+                </p>
+                <p className="text-md font-bold sm:text-md text-white mb-2">
+                  {event.description}
+                </p>
+                <p className="text-md font-bold sm:text-md text-darkGrey mb-2">
+                  Venue: {event.location}
+                </p>
+                <Link href={event.link}>
+                  <button className="inline-block text-primaryOrange bg-darkGrey py-1 px-4 sm:py-2 sm:px-6 rounded-full font-bold hover:bg-white text-primaryOrange transition duration-200">
+                    Explore
+                  </button>
+                </Link>
+              </div>
             </div>
           ))}
         </div>
